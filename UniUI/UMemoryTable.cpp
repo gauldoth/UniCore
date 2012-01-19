6842673dd9082df5b431ae35b0964c80d65b95c6
unigauldoth <unigauldoth@gmail.com>
2012-01-19 08:38:33 +0000

    增加内存视图相关代码，用于查看内存。 未完成。

---
+++ UniUI/UMemoryTable.cpp
@@ -0,0 +1,18 @@
+﻿#include "UMemoryTable.h"
+
+namespace uni
+{
+
+UMemoryTable::UMemoryTable( QWidget *parent /*= 0*/ )
+:QTableView(parent)
+{
+    //setHorizontalHeader();
+    
+}
+
+UMemoryTable::~UMemoryTable()
+{
+
+}
+
+}//namespace uni
---
+++ UniUI/UMemoryView.cpp
@@ -0,0 +1,40 @@
+﻿#include "UMemoryView.h"
+
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+
+#include "UMemoryModel.h"
+#include "UMemoryTable.h"
+
+namespace uni
+{
+
+UMemoryView::UMemoryView( QWidget *parent /*= 0*/ )
+:QWidget(parent)
+{
+    setupUI();
+}
+
+UMemoryView::~UMemoryView()
+{
+
+}
+
+void UMemoryView::setupUI()
+{
+    QHBoxLayout *layout = new QHBoxLayout;
+    addressLabel_ = new QLabel("Address:");
+    layout->addWidget(addressLabel_);
+    addressEdit_ = new QLineEdit();
+    layout->addWidget(addressEdit_);
+    QVBoxLayout *layout1 = new QVBoxLayout;
+    layout1->addLayout(layout);
+    memoryModel_ = new UMemoryModel(this);
+    memoryTable_ = new UMemoryTable;
+    memoryTable_->setModel(memoryModel_);
+    layout1->addWidget(memoryTable_);
+    setLayout(layout1);
+}
+
+}//namespace uni
