48eeecd0ca9185ce74898fd6ea0d4e204f5909ca
unigauldoth <unigauldoth@gmail.com>
2012-02-08 09:20:48 +0000

    增加UPacketView，是用于查看封包的类。

---
+++ UniUI/UPacketListModel.h
@@ -0,0 +1,27 @@
+﻿/*! \file UPacketListModel.h
+    \brief 提供封包ID及其对应信息的数据模型。
+
+    \author uni
+    \date 2012-2-8
+*/
+#ifndef UNIUI_UPACKET_LIST_MODEL_H
+#define UNIUI_UPACKET_LIST_MODEL_H
+
+#include <QAbstractTableModel>
+
+namespace uni
+{
+
+//! UPacketList使用的Model。
+class UPacketListModel : public QAbstractTableModel
+{
+    Q_OBJECT
+
+public:
+    explicit UPacketListModel(QWidget *parent = 0);
+    virtual ~UPacketListModel();
+};
+
+}//namespace uni
+
+#endif//UNIUI_UPACKET_LIST_MODEL_H
---
+++ UniUI/UPacketView.cpp
@@ -0,0 +1,29 @@
+﻿#include "UPacketView.h"
+
+
+namespace uni
+{
+
+UPacketView::UPacketView( QWidget *parent /*= 0*/ )
+:QWidget(parent)
+{
+
+}
+
+UPacketView::~UPacketView()
+{
+
+}
+
+void UPacketView::createPacketListGroupBox()
+{
+    
+}
+
+void UPacketView::setupUI()
+{
+    createPacketListGroupBox();
+
+}
+
+}//namespace uni
