4ee394b2288e4c2bc3b57d98513beab833586285
unigauldoth <unigauldoth@gmail.com>
2012-03-12 09:28:13 +0000

    增加了一个简单的Buffer对象。

---
+++ UniCore/UBuffer.h
@@ -0,0 +1,41 @@
+﻿/*! \file UBuffer.h
+    \brief 简单的Buffer类。
+
+    \author uni
+    \date 2012-3-12
+*/
+#ifndef UNICORE_UBUFFER_H
+#define UNICORE_UBUFFER_H
+
+#include <string>
+
+namespace uni
+{
+
+//! 缓冲区简单抽象。
+/*!
+    
+*/
+class UBuffer
+{
+public:
+    UBuffer() {}
+    virtual ~UBuffer() {}
+    const char *data() const {return buf_.c_str();}
+    int size() const {return buf_.size();}
+    void appendChar(char data) {buf_.push_back(data);}
+    void appendShort(short data)
+    {
+        buf_.append((char *)&data,sizeof(data));
+    }
+    void appendInt(int data)
+    {
+        buf_.append((char *)&data,sizeof(data));
+    }
+private:
+    std::string buf_;
+};
+
+}//namespace uni
+
+#endif//UNICORE_UBUFFER_H
