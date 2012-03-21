39c2b3bd01fcf723a58fb95073d0607849d20b8d
unigauldoth@gmail.com <unigauldoth@gmail.com@dc51e77d-ff88-7ab5-d8f9-136e1542a5dd>
2012-03-21 09:24:51 +0000

    准备向UPacketView（封包视图）中加入UDisplayList（显示方案列表）。 新增一个Lua解释器界面。

---
+++ trunk/UniUI/UPacketDisplayList.cpp
@@ -0,0 +1,17 @@
+﻿#include "UPacketDisplayList.h"
+
+namespace uni
+{
+
+UPacketDisplayList::UPacketDisplayList( QWidget *parent /*= 0*/ )
+:QWidget(parent)
+{
+
+}
+
+UPacketDisplayList::~UPacketDisplayList()
+{
+
+}
+
+}//namespace uni
