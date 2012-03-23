0f5eb2ed54586fe509b3358007d0208a3efc344f
unigauldoth <unigauldoth@gmail.com>
2012-03-23 16:09:31 +0000

    增加ULuaInterpreter的开发测试用项目。

---
+++ ULuaInterpreter_Dev/ULuaInterpreter_Dev.cpp
@@ -0,0 +1,12 @@
+#include "ULuaInterpreter_Dev.h"
+
+ULuaInterpreter_Dev::ULuaInterpreter_Dev(QWidget *parent, Qt::WFlags flags)
+    : QMainWindow(parent, flags)
+{
+    ui.setupUi(this);
+}
+
+ULuaInterpreter_Dev::~ULuaInterpreter_Dev()
+{
+
+}
---
+++ ULuaInterpreter_Dev/main.cpp
@@ -0,0 +1,14 @@
+#include <QtGui/QApplication>
+#include "ULuaInterpreter_Dev.h"
+
+#include "../UniUI/ULuaInterpreter.h"
+
+using namespace uni;
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    ULuaInterpreter w;
+    w.show();
+    return a.exec();
+}
