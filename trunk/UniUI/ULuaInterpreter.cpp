a39807471dd03c0300b28eea0b76125cc98342e9
unigauldoth@gmail.com <unigauldoth@gmail.com@dc51e77d-ff88-7ab5-d8f9-136e1542a5dd>
2012-03-23 09:42:54 +0000

    继续完善Lua解释器窗口。

--- trunk/UniUI/ULuaInterpreter.cpp
+++ trunk/UniUI/ULuaInterpreter.cpp
@@ -1,5 +1,8 @@
 ﻿#include "ULuaInterpreter.h"
 
+#include "../UniCore/ULua.h"
+#include "../UniCore/ULog.h"
+
 namespace uni
 {
 
@@ -9,13 +12,22 @@ ULuaInterpreter::ULuaInterpreter(lua_State *L /*= 0*/, QWidget *parent /*= 0*/)
 {
     if(!L_)
     {
-        
+        L_ = lua_open();
+        if(!L_)
+        {
+            UERROR("Lua解释器")<<"无法创建Lua状态，内存不足。";
+            return;
+        }
+        lua_gc(L_,LUA_GCSTOP,0);
+        luaL_openlibs(L_);
+        RegisterCommonLuaFunctions(L_);
+        lua_gc(L_,LUA_GCRESTART,0);
     }
 }
 
 ULuaInterpreter::~ULuaInterpreter()
 {
-
+    lua_close(L_);
 }
 
 }//namespace uni
