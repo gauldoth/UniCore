9b5cd24f0a696e8d38631a4b1e07bf5cc4c33c2f
unigauldoth@gmail.com <unigauldoth@gmail.com@dc51e77d-ff88-7ab5-d8f9-136e1542a5dd>
2012-06-01 09:23:16 +0000

    增加用于显示单条封包内容的窗口。

---
+++ trunk/UniUI/UPacketContentView.cpp
@@ -0,0 +1,39 @@
+﻿#include "UPacketContentView.h"
+
+#include <QHBoxLayout>
+#include <QSplitter>
+#include <QTextEdit>
+
+namespace uni
+{
+
+
+UPacketContentView::UPacketContentView( QWidget *parent /*= 0*/ )
+:QDialog(parent)
+{
+    packetContentEdit_ = new QTextEdit(this);
+    packetContentEdit_->setMinimumWidth(320);
+    packetTextEdit_ = new QTextEdit(this);
+    packetTextEdit_->setMinimumWidth(220);
+    QSplitter *splitter = new QSplitter(Qt::Horizontal,this);
+    splitter->addWidget(packetContentEdit_);
+    splitter->addWidget(packetTextEdit_);
+
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(splitter);
+    setLayout(layout);
+}
+
+UPacketContentView::~UPacketContentView()
+{
+
+}
+
+
+void UPacketContentView::setPacket( UPacketView::PacketData *packetData )
+{
+    packetContentEdit_->setText(packetData->contentTooltip);
+    packetTextEdit_->setText(packetData->textTooltip);
+}
+
+}//namespace uni
