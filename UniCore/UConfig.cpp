669250eb5cbb8141d20eb8a6bf6444f1813f665c
unigauldoth <unigauldoth@gmail.com>
2012-06-13 15:15:21 +0000

    UConfig简化，打算先仿造QSetting写个简单的版本。

--- UniCore/UConfig.cpp
+++ UniCore/UConfig.cpp
@@ -5,35 +5,48 @@ using namespace std;
 namespace uni
 {
 
-UConfig *UConfig::instance_ = 0;
-
-UConfig::UConfig()
-{
-
-}
-
-void UConfig::setFile( const std::string &filePath )
+UConfig::UConfig( const std::string &filePath )
+:filePath_(filePath)
 {
 
 }
 
 void UConfig::set( const std::string &key,const std::string &value )
 {
+    string sec;  //写配置档时的section name。  
+    size_t slashIndex = key.find('/');
+    if(slashIndex != string::npos)
+    {
+        sec = key.substr(0,slashIndex);
+        key.erase(0,slashIndex);
+    }
+    if(sec.empty())
+    {
+        sec = "general";
+    }
 
+    if(!WritePrivateProfileStringA(sec.c_str(),key.c_str(),value,filePath_))
+    {
+        UERROR("UniCore")<<"WritePrivateProfileStringA失败。"<<lasterr;
+    }
 }
 
 std::string UConfig::get( const std::string &key )
 {
-    return string();
-}
-
-UConfig & UConfig::instance()
-{
-    if(!instance_)
+    string sec;  //写配置档时的section name。  
+    size_t slashIndex = key.find('/');
+    if(slashIndex != string::npos)
     {
-        instance_ = new UConfig;
+        sec = key.substr(0,slashIndex);
+        key.erase(0,slashIndex);
     }
-    return *instance_;
+    if(sec.empty())
+    {
+        sec = "general";
+    }
+
+    //if(!GetPrivateProfileString(sec.c_str(),key.c_str(),"",))
+    return string();
 }
 
 }//namespace uni
