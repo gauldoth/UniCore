6d3274437ff6ca763ec9ffdca25ea46419ca2497
unigauldoth <unigauldoth@gmail.com>
2012-06-14 09:28:05 +0000

    细微改动。

--- UniCore/UConfig.cpp
+++ UniCore/UConfig.cpp
@@ -1,5 +1,10 @@
 ﻿#include "UConfig.h"
 
+#define WIN32_LEAN_AND_MEAN
+#include "Windows.h"
+
+#include "ULog.h"
+
 using namespace std;
 
 namespace uni
@@ -13,19 +18,24 @@ UConfig::UConfig( const std::string &filePath )
 
 void UConfig::set( const std::string &key,const std::string &value )
 {
-    string sec;  //写配置档时的section name。  
+    string sectionName;  //写配置档时的section name。 
+    string keyName;
     size_t slashIndex = key.find('/');
     if(slashIndex != string::npos)
     {
-        sec = key.substr(0,slashIndex);
-        key.erase(0,slashIndex);
+        sectionName = key.substr(0,slashIndex);
+        keyName = key.substr(slashIndex+1);
     }
-    if(sec.empty())
+    if(sectionName.empty())
     {
-        sec = "general";
+        sectionName = "general";
+    }
+    if(keyName.empty())
+    {
+        keyName = key;
     }
 
-    if(!WritePrivateProfileStringA(sec.c_str(),key.c_str(),value,filePath_))
+    if(!WritePrivateProfileStringA(sectionName.c_str(),keyName.c_str(),value.c_str(),filePath_.c_str()))
     {
         UERROR("UniCore")<<"WritePrivateProfileStringA失败。"<<lasterr;
     }
@@ -33,16 +43,21 @@ void UConfig::set( const std::string &key,const std::string &value )
 
 std::string UConfig::get( const std::string &key )
 {
-    string sec;  //写配置档时的section name。  
+    string sectionName;  //写配置档时的section name。 
+    string keyName;
     size_t slashIndex = key.find('/');
     if(slashIndex != string::npos)
     {
-        sec = key.substr(0,slashIndex);
-        key.erase(0,slashIndex);
+        sectionName = key.substr(0,slashIndex);
+        keyName = key.substr(slashIndex+1);
     }
-    if(sec.empty())
+    if(sectionName.empty())
     {
-        sec = "general";
+        sectionName = "general";
+    }
+    if(keyName.empty())
+    {
+        keyName = key;
     }
 
     //if(!GetPrivateProfileString(sec.c_str(),key.c_str(),"",))
