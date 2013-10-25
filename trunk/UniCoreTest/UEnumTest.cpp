ed145b3bdd79fbbb7f7563e3d8b9cf576b1d4603
unigauldoth@gmail.com <unigauldoth@gmail.com@dc51e77d-ff88-7ab5-d8f9-136e1542a5dd>
2013-10-25 10:14:57 +0000

    增加UEnum. 用于转换枚举到字符串.

---
+++ trunk/UniCoreTest/UEnumTest.cpp
@@ -0,0 +1,50 @@
+﻿#include "stdafx.h"
+
+#include "gtest/gtest.h"
+#include "../UniCore/UEnum.h"
+
+enum RWBY
+{
+    Ross,
+    Weiss,
+    Yang,
+    Blake,
+};
+
+UENUM_BEGIN(RWBY)
+    UENUM_ENTRY(Ross)
+    UENUM_ENTRY(Weiss)
+    UENUM_ENTRY(Yang)
+    UENUM_ENTRY(Blake)
+UENUM_END
+
+enum Fruit
+{
+    Apple,
+    Banana,
+};
+
+UENUM_BEGIN(Fruit)
+    UENUM_ENTRY(Apple)
+    UENUM_ENTRY(Banana)
+UENUM_END
+
+TEST(UEnumTest,EnumToString_NonIntrusive_ConvertSuccess)
+{
+    RWBY rwby;
+    rwby = Ross;
+    EXPECT_EQ(L"Ross",e2s(rwby));
+    rwby = Weiss;
+    EXPECT_EQ(L"Weiss",e2s(rwby));
+    rwby = Yang;
+    EXPECT_EQ(L"Yang",e2s(rwby));
+    rwby = Blake;
+    EXPECT_EQ(L"Blake",e2s(rwby));
+}
+
+TEST(UEnumTest,e2s_TwoEnums_ConvertSuccess)
+{
+    Fruit f;
+    f = Apple;
+    EXPECT_EQ(L"Apple",e2s(f));
+}
