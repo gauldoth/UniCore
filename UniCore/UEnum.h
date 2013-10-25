8cdd5187e034df47d2c1d1a08145ae247d5902b7
unigauldoth <unigauldoth@gmail.com>
2013-10-25 10:14:57 +0000

    增加UEnum. 用于转换枚举到字符串.

---
+++ UniCore/UEnum.h
@@ -0,0 +1,47 @@
+﻿/*! \file UDebug.h
+    \brief 提供了调试常用的一些函数。
+
+    \author     uni
+    \date       2013-10-25
+*/
+#ifndef UNICORE_UENUM_H
+#define UNICORE_UENUM_H
+
+#include <map>
+
+static std::map<std::string,std::map<int,std::wstring> > g_enum_string_datas;
+
+template<typename T>
+inline void populate_enum_strings()
+{
+
+}
+
+#define UENUM_BEGIN(enum_name) \
+    template<>\
+    inline void populate_enum_strings<enum_name>() \
+    { \
+    std::string name = "enum "; \
+    name += #enum_name; \
+    if(g_enum_string_datas.count(name)) \
+    { \
+        return; \
+    } 
+
+#define UENUM_ENTRY(entry) \
+    g_enum_string_datas[name][entry] = L#entry; 
+
+#define UENUM_ENTRY_VALUE(entry,value) \
+    g_enum_string_datas[name][entry] = L#value;
+
+#define UENUM_END \
+    }
+
+template<typename enum_name>
+inline const std::wstring e2s(enum_name e)
+{
+    populate_enum_strings<enum_name>();
+    return g_enum_string_datas[typeid(enum_name).name()][e].c_str();
+}
+
+#endif//UNICORE_UENUM_H
