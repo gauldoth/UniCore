57a71862a2ee414ac79e8c5349dbed8641963422
unigauldoth@gmail.com <unigauldoth@gmail.com@dc51e77d-ff88-7ab5-d8f9-136e1542a5dd>
2013-12-24 14:45:15 +0000

    增加GetTempPathW函数.

---
+++ trunk/UniCore/USystem.cpp
@@ -0,0 +1,19 @@
+﻿#include "USystem.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include "Windows.h"
+
+std::wstring GetTempPathW()
+{
+  const int bufSize = MAX_PATH+1;
+  wchar_t buf[bufSize] = L"";
+  if(GetTempPathW(bufSize,buf))
+  {
+    return buf;
+  }
+  else
+  {
+    return std::wstring();
+  }
+}
+
---
+++ trunk/UniCore/USystem.h
@@ -0,0 +1,16 @@
+﻿/*! \file USystem.h
+    \brief 包含了一些系统API的简单封装.
+
+    \date 2013-12-24
+*/
+#ifndef UNICORE_USYSTEM_H
+#define UNICORE_USYSTEM_H
+
+#define AUTO_LINK_LIB_NAME "UniCore"
+#include "AutoLink.h"
+
+#include <string>
+
+std::wstring GetTempPathW();
+
+#endif//UNICORE_USYSTEM_H
