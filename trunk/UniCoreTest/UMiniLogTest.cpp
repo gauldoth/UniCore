01e12022491b6586de287c8ccfd2e31035a482d6
unigauldoth@gmail.com <unigauldoth@gmail.com@dc51e77d-ff88-7ab5-d8f9-136e1542a5dd>
2014-03-21 09:55:35 +0000

    UMiniLog第一版.

---
+++ trunk/UniCoreTest/UMiniLogTest.cpp
@@ -0,0 +1,12 @@
+﻿#include "stdafx.h"
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include <vector>
+#include <string>
+#include "../UniCore/UMiniLog.h"
+
+TEST(UMiniLogTest,Compile_OK)
+{
+    MINILOG<<"test";
+}
